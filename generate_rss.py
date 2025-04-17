bb16ff0eb4e929960ec787b08ac8fa863bec7cc9
AlexHetzelfde <mightygamer500@gmail.com>
2025-04-17 14:45:21 +0200

    Update generate_rss.py

--- generate_rss.py
+++ generate_rss.py
@@ -1,23 +1,22 @@
-import feedgen.feed
-from datetime import datetime
+items = [
+    {
+        'date': '17-04-2025',
+        'title': 'Bezwaarbrief Boschjesstraat',
+        'link': 'https://zaanstad.bestuurlijkeinformatie.nl/Reports/Item/1a414256-230b-4a86-81fc-e2285f9ba8ad',
+        'description': 'Ter kennisname, afdoening door college B&W, de raad ontvangt een kopie van de afdoening'
+    },
+    # Voeg hier andere items toe in hetzelfde formaat
+]
 
-# Maak een nieuwe RSS-feed aan
-feed = feedgen.feed.FeedGenerator()
-feed.title('Zaanstad iBabs Updates')
-feed.link(href='https://www.zaanstad.nl')
-feed.description('Updates van de Zaanstad iBabs pagina.')
-feed.language('nl')
+for item in items:
+    entry = feed.add_entry()
+    entry.title(item['title'])
+    entry.link(href=item['link'])
+    entry.pubDate(datetime.strptime(item['date'], '%d-%m-%Y'))
+    entry.description(item['description'])
 
-# Voeg een item toe aan de feed
-entry = feed.add_entry()
-entry.title('Bezwaarbrief Boschjesstraat')
-entry.link(href='https://zaanstad.bestuurlijkeinformatie.nl/Reports/Item/1a414256-230b-4a86-81fc-e2285f9ba8ad')
-entry.pubDate(datetime.strptime('17-04-2025', '%d-%m-%Y'))
-entry.description('Ter kennisname, afdoening door college B&W, de raad ontvangt een kopie van de afdoening')
-
-# Genereer het RSS-bestand
 rss_feed = feed.rss_str()
 
-# Sla het op als een XML bestand
+# Opslaan als XML bestand
 with open('rss.xml', 'wb') as f:
     f.write(rss_feed)
