b9fb105e52257f86b309bde7f36fc3bdaf04956a
AlexHetzelfde <mightygamer500@gmail.com>
2025-04-17 14:31:53 +0200

    Update generate_rss.py

--- generate_rss.py
+++ generate_rss.py
@@ -1,72 +1,23 @@
-{
-  "rss": {
-    "version": "2.0",
-    "channel": {
-      "title": "Zaanstad iBabs Updates",
-      "link": "https://www.zaanstad.nl",
-      "description": "Updates van de Zaanstad iBabs pagina.",
-      "item": [
-        {
-          "title": "Leerlingen- en doelgroepenvervoer Zaanstad",
-          "link": "https://www.zaanstad.nl/iBabs?id=fbc6fe3a-aa98-4372-9eef-e6c30093321e",
-          "pubDate": "Thu, 17 Apr 2025 00:00:00 +0000",
-          "description": "Raadsinformatiebrief - Actieve informatievoorziening"
-        },
-        {
-          "title": "Gevolgen niet aannemen parkeervisie auto- en fietsparkeren 2024",
-          "link": "https://www.zaanstad.nl/iBabs?id=f7cb2c0d-ede9-4355-88f6-41a657a677fb",
-          "pubDate": "Thu, 17 Apr 2025 00:00:00 +0000",
-          "description": "Raadsinformatiebrief - Actieve informatievoorziening"
-        },
-        {
-          "title": "Actieagenda MKB en starters",
-          "link": "https://www.zaanstad.nl/iBabs?id=450eba78-af53-4abd-98ee-69b5847369ed",
-          "pubDate": "Wed, 16 Apr 2025 00:00:00 +0000",
-          "description": "Raadsinformatiebrief - Actieve informatievoorziening"
-        },
-        {
-          "title": "Besluitenlijst B&W 08-04-2025",
-          "link": "https://www.zaanstad.nl/iBabs?id=40e30a04-76b2-40ff-96cb-0103f9b8a08d",
-          "pubDate": "Tue, 15 Apr 2025 00:00:00 +0000",
-          "description": "Besluitenlijst B&W - Ter kennisname"
-        },
-        {
-          "title": "Fietstunnel N515 Zuiderweg",
-          "link": "https://www.zaanstad.nl/iBabs?id=8bad8c2a-0124-4c6b-98d0-f387911dead8",
-          "pubDate": "Tue, 15 Apr 2025 00:00:00 +0000",
-          "description": "Raadsinformatiebrief - Actieve informatievoorziening"
-        },
-        {
-          "title": "Prijsdifferentiatie Woningbouw 2025",
-          "link": "https://www.zaanstad.nl/iBabs?id=749f3b4d-5b87-4654-a1aa-389a23c65165",
-          "pubDate": "Tue, 15 Apr 2025 00:00:00 +0000",
-          "description": "Raadsinformatiebrief - Actieve informatievoorziening"
-        },
-        {
-          "title": "Procesbesluit 2025",
-          "link": "https://www.zaanstad.nl/iBabs?id=bf1617a9-0697-4145-85aa-d4e85d02d699",
-          "pubDate": "Tue, 15 Apr 2025 00:00:00 +0000",
-          "description": "Raadsinformatiebrief - Actieve informatievoorziening"
-        },
-        {
-          "title": "Resultaat tender Poort van Zaandam Oost",
-          "link": "https://www.zaanstad.nl/iBabs?id=25bb5ccb-5d89-470a-be89-9ea029a2c014",
-          "pubDate": "Fri, 10 Apr 2025 00:00:00 +0000",
-          "description": "Raadsinformatiebrief - Actieve informatievoorziening"
-        },
-        {
-          "title": "Wijziging Uitvoeringsprogramma Vergunningverlening, Toezicht en Handhaving 2025-2026",
-          "link": "https://www.zaanstad.nl/iBabs?id=1edc6e30-0848-4473-98f0-59d60306a1d0",
-          "pubDate": "Fri, 10 Apr 2025 00:00:00 +0000",
-          "description": "Aanbiedingsbrief - Ter kennisname"
-        },
-        {
-          "title": "Overzicht activiteiten verduurzaming bestaande Zaanse woningvoorraad en gemeentelijk vastgoed",
-          "link": "https://www.zaanstad.nl/iBabs?id=ea31ac2d-f834-413f-89ec-e02ed5666f7f",
-          "pubDate": "Fri, 10 Apr 2025 00:00:00 +0000",
-          "description": "Raadsinformatiebrief - Actieve informatievoorziening"
-        }
-      ]
-    }
-  }
-}
+import feedgen.feed
+from datetime import datetime
+
+# Maak een nieuwe RSS-feed aan
+feed = feedgen.feed.FeedGenerator()
+feed.title('Zaanstad iBabs Updates')
+feed.link(href='https://www.zaanstad.nl')
+feed.description('Updates van de Zaanstad iBabs pagina.')
+feed.language('nl')
+
+# Voeg een item toe aan de feed
+entry = feed.add_entry()
+entry.title('Bezwaarbrief Boschjesstraat')
+entry.link(href='https://zaanstad.bestuurlijkeinformatie.nl/Reports/Item/1a414256-230b-4a86-81fc-e2285f9ba8ad')
+entry.pubDate(datetime.strptime('17-04-2025', '%d-%m-%Y'))
+entry.description('Ter kennisname, afdoening door college B&W, de raad ontvangt een kopie van de afdoening')
+
+# Genereer het RSS-bestand
+rss_feed = feed.rss_str()
+
+# Sla het op als een XML bestand
+with open('rss.xml', 'wb') as f:
+    f.write(rss_feed)
